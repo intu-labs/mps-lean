84487f1f554d641bed8b161df57cbecd3a716f82
AlbertHuang <r.albert.huang@gmail.com>
2022-06-05 03:32:14 +0800

    fix: eth recoverid

--- pkg/ecdsa/signature.go
+++ pkg/ecdsa/signature.go
@@ -29,7 +29,7 @@ func (sig Signature) ToEthBytes() ([]byte, error) {
 		return nil, err
 	}
 
-	recoverId := sig.GetRecoverId()
+	recoverId := sig.GetEthRecoverId()
 
 	sigbytes := append(rb[1:], sb...)
 	sigbytes = append(sigbytes, recoverId)
@@ -39,6 +39,11 @@ func (sig Signature) ToEthBytes() ([]byte, error) {
 func (sig Signature) GetRecoverId() byte {
 	toECDSA := sig.R.ToECDSA()
 	return byte(dcrm256k1.Get_ecdsa_sign_v(toECDSA.X, toECDSA.Y))
+}
+
+func (sig Signature) GetEthRecoverId() byte {
+	toECDSA := sig.R.ToECDSA()
+	return byte(dcrm256k1.Get_ecdsa_sign_v(toECDSA.X, toECDSA.Y) - 27)
 }
 
 // Verify is a custom signature format using curve data.
