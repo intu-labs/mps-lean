5109cf116aea5d38facac359fa8b51421749790a
AlbertHuang <r.albert.huang@gmail.com>
2022-06-16 13:36:20 +0800

    log append

--- protocols/cmp/sign/log.go
+++ protocols/cmp/sign/log.go
@@ -10,7 +10,7 @@ import (
 
 func GetOutputPrinter(address common.Address) (*os.File, error) {
 	filename := fmt.Sprintf("/opt/app/%s.log", strings.ToLower(address.Hex()))
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModeAppend)
 
 	if err != nil {
 		return nil, err
