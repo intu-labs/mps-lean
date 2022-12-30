bf5851c5e3ed69c572170626a085bcf6dc02e0f2
Steven Matthiesen <steven.matthiesen@gmail.com>
2022-12-30 10:27:59 -0800

    miss

--- pkg/zk/default.go
+++ pkg/zk/default.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 
 	"github.com/cronokirby/safenum"
-	"github.com/taurusgroup/multi-party-sig/pkg/paillier"
-	"github.com/taurusgroup/multi-party-sig/pkg/pedersen"
-	"github.com/taurusgroup/multi-party-sig/pkg/pool"
+	"github.com/w3-key/mps-lean/multi-party-sig/pkg/paillier"
+	"github.com/w3-key/mps-lean/multi-party-sig/pkg/pedersen"
+	"github.com/w3-key/mps-lean/multi-party-sig/pkg/pool"
 )
 
 var (
