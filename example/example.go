beafe85afb9711848f2e04aa3819a0b65eee399d
Toufic <touficbatrice@gmail.com>
2023-02-15 18:08:33 +0100

    wip

--- example/example.go
+++ example/example.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/w3-key/mps-lean/pkg/ecdsa"
 	"github.com/w3-key/mps-lean/pkg/math/curve"
+	"github.com/w3-key/mps-lean/pkg/paillier"
 	"github.com/w3-key/mps-lean/pkg/party"
 	"github.com/w3-key/mps-lean/pkg/pool"
 	"github.com/w3-key/mps-lean/pkg/protocol"
@@ -377,3 +378,16 @@ func main() {
 
 	wg.Wait()
 }
+
+func ChangeConfig(Paillier *paillier.SecretKey, id party.ID, ids party.IDSlice, n int, t int, shares map[party.ID]*curve.Scalar, c *cmp.Config) (*cmp.Config, error) {
+
+	shares := [c6f41b6941ba2e10560bae52d5c16a215ce9ce8c2dd0bccb846ad6996c4f2190, 851c366b5b17c267ad3fe27301324b387659ab2c3c20a4eab90c5ee6da01b48d, d0fed3d9463bd8cb21c0905030d3a2473c5355e2baa7c7bd770d9ffdff0dba85, cacdf8283da71a30b3611d99565e66491a7902fa4ac5c7ecd800ed2e15619802, 92bba7cd7bda2f8e61f4effd638b8e38371bc3a43d22e85db54af8532721f2cf]
+	
+	c.Threshold = t
+	c.ECDSA = *shares[id]
+	c.ID = id
+	c.Paillier = Paillier
+
+	return c, nil
+}
+
