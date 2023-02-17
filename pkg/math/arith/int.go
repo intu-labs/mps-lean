5f00fd98ddf6e5a97b11a41abb36d54ea6ebe077
Steven Matthiesen <steven.matthiesen@gmail.com>
2023-02-17 03:28:58 -0800

    wow

--- pkg/math/arith/int.go
+++ pkg/math/arith/int.go
@@ -3,12 +3,12 @@ package arith
 import (
 	"math/big"
 
-	"github.com/cronokirby/safenum"
+	"github.com/cronokirby/saferith"
 	"github.com/w3-key/mps-lean/pkg/params"
 )
 
 // IsValidNatModN checks that ints are all in the range [1,…,N-1] and co-prime to N.
-func IsValidNatModN(N *safenum.Modulus, ints ...*safenum.Nat) bool {
+func IsValidNatModN(N *saferith.Modulus, ints ...*saferith.Nat) bool {
 	for _, i := range ints {
 		if i == nil {
 			return false
@@ -46,7 +46,7 @@ func IsValidBigModN(N *big.Int, ints ...*big.Int) bool {
 }
 
 // IsInIntervalLEps returns true if n ∈ [-2ˡ⁺ᵉ,…,2ˡ⁺ᵉ].
-func IsInIntervalLEps(n *safenum.Int) bool {
+func IsInIntervalLEps(n *saferith.Int) bool {
 	if n == nil {
 		return false
 	}
@@ -54,7 +54,7 @@ func IsInIntervalLEps(n *safenum.Int) bool {
 }
 
 // IsInIntervalLPrimeEps returns true if n ∈ [-2ˡ'⁺ᵉ,…,2ˡ'⁺ᵉ].
-func IsInIntervalLPrimeEps(n *safenum.Int) bool {
+func IsInIntervalLPrimeEps(n *saferith.Int) bool {
 	if n == nil {
 		return false
 	}
--- protocols/cmp/keygen/round3.go
+++ protocols/cmp/keygen/round3.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/cronokirby/safenum"
+	"github.com/cronokirby/saferith"
 	"github.com/w3-key/mps-lean/pkg/hash"
 	"github.com/w3-key/mps-lean/pkg/math/curve"
 	"github.com/w3-key/mps-lean/pkg/math/polynomial"
@@ -38,11 +38,11 @@ type broadcast3 struct {
 	SchnorrCommitments *zksch.Commitment
 	ElGamalPublic      curve.Point
 	// N Paillier and Pedersen N = p•q, p ≡ q ≡ 3 mod 4
-	N *safenum.Modulus
+	N *saferith.Modulus
 	// S = r² mod N
-	S *safenum.Nat
+	S *saferith.Nat
 	// T = Sˡ mod N
-	T *safenum.Nat
+	T *saferith.Nat
 	// Decommitment = uᵢ decommitment bytes
 	Decommitment hash.Decommitment
 }
